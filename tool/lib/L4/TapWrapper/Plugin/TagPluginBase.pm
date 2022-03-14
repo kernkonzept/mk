39675a14ff774e1564f11876b8bee2a3a3834dff
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2022-03-14 00:00:00 +0000

    tap-wrapper: Better block support of tag plugins
    Sometimes tag-based plugins must be made aware if a new block is started
    or must be notified if a block is terminated. Add a function that they
    can hook into to detect such a state.
    
    Change-Id: I1d8bf06d103504d808f5b0b0689f0d037c1d7fed

--- tool/lib/L4/TapWrapper/Plugin/TagPluginBase.pm
+++ tool/lib/L4/TapWrapper/Plugin/TagPluginBase.pm
@@ -27,6 +27,7 @@ sub check_start {
   $self->{in_block} = 1;
   $self->{block_prefix} = $1;
   $self->{block_info} = $2;
+  $self->start_block($2);
 
   # Inhibit unless we already do because we require more blocks
   $self->inhibit_exit() unless $self->{args}{require_blocks};
@@ -37,6 +38,7 @@ sub check_end {
   $self->{clean_line} =~ s/^\Q$self->{block_prefix}\E//;
   return unless shift =~ m/^\Q$self->{block_prefix}\E@@ $self->{args}{tag} BLOCK >@/;
   $self->{in_block} = 0;
+  $self->end_block();
   $self->permit_exit()
     unless $self->{args}{require_blocks} && --$self->{args}{require_blocks};
 }
@@ -45,6 +47,9 @@ sub has_tag {
   my $self = shift;
   return $self->{clean_line} =~ s/^.*@@ $self->{args}{tag}://;
 }
+
+sub start_block {}
+sub end_block {}
 
 1;
 
@@ -89,6 +94,22 @@ expressions.
 An optional numerical argument indicating how many blocks must arrive before the
 plugin stops inhibiting test exit. The default is zero zero indicating that the
 plugin does not inhibit exits and works opportunistically.
+
+=back
+
+=head1 Functions
+
+The following functions are implemented that plugins based on this can hook
+into:
+
+=item C<end_block>
+
+Is called whenever the current block is terminated.
+
+=item C<start_block>
+
+Is called for every new block. The extra block info (see capture in the block
+start regex) is passed as a parameter.
 
 =back
 
