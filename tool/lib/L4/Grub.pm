8942f74ed7ad96b2753f00a6d501e7c5f6622d79
Martin Decky <martin.decky@kernkonzept.com>
2023-07-17 00:00:00 +0000

    Grub.pm: Add support for creating hybrid ISO/UEFI images
    The grub-mkrescue tool actually allows for both PC and UEFI modules to
    be installed simultaneously and creates a hybrid PC/UEFI ISO image in
    that case.
    
    If we detect that grub-mkrescue neither creates an exclusive PC ISO nor
    an exclusive UEFI ISO, enable the hybrid ISO detection as the last
    resort.
    
    Change-Id: I90bd16f1dbbebd1a8c80822eca6f4198fff3b69c

--- tool/lib/L4/Grub.pm
+++ tool/lib/L4/Grub.pm
@@ -129,21 +129,32 @@ sub grub2_mkisofs($$@)
   $opt = " -as mkisofs" unless $?;
 
 
-  # Check whether the installed grub works for either PC or UEFI. Assumes
-  # that only one variant is installed, as we do not really know how to
-  # select the variant.
+  # Check whether the installed GRUB works for either PC or UEFI.
   my $is_pc;
   my $is_efi;
+  my $is_hybrid;
+
   system("$mkr --install-modules=vbe --output=/dev/null /dev/null -f >/dev/null 2>&1");
   $is_pc = $? == 0;
 
   system("$mkr --install-modules=lsefi --output=/dev/null /dev/null -f >/dev/null 2>&1");
   $is_efi = $? == 0;
 
-  die "grub-mkrescue does not seem to work for either PC or UEFI targets. GRUB not (properly) installed?"
-    if not $is_pc and not $is_efi;
+  if (not $is_pc and not $is_efi)
+    {
+      # As the last resort, check if GRUB is able to create a hybrid PC/UEFI image.
+      system("$mkr --install-modules=acpi --output=/dev/null /dev/null -f >/dev/null 2>&1");
+      $is_hybrid = $? == 0;
+    }
 
-  print "Note: Take care, ISO can be PC or UEFI\n" if $is_efi and $is_pc;
+  die "grub-mkrescue does not seem to work for either PC or UEFI targets. GRUB not (properly) installed?"
+    if not $is_pc and not $is_efi and not $is_hybrid;
+
+  if ($is_efi and $is_pc)
+    {
+      print "\nWarning: Unable to determine whether grub-mkrescue creates PC or UEFI images.\n";
+      print "         Use your own discretion.\n\n";
+    }
 
   my $cmd = "$mkr --output=\"$isofilename\" $dir ".
             join(' ', @morefiles)." --$opt -f";
@@ -152,8 +163,17 @@ sub grub2_mkisofs($$@)
   # grub-mkrescue does not propagate internal tool errors
   die "Failed to create ISO" unless -e $isofilename;
 
-  print "Note: Created UEFI ISO, remember to add '-bios /usr/share/qemu/OVMF.fd' to your QEMU call\n\n"
-    if $is_efi;
+  if ($is_efi)
+    {
+      print "Note: Created UEFI ISO. Remember to add '-bios /usr/share/qemu/OVMF.fd'\n";
+      print "      (or similar) to your QEMU call.\n\n";
+    }
+
+  if ($is_hybrid)
+    {
+      print "Note: Created hybrid PC/UEFI ISO. Remember to add '-bios /usr/share/qemu/OVMF.fd'\n";
+      print "      (or similar) to your QEMU call to boot it in UEFI mode.\n\n";
+    }
 }
 
 
