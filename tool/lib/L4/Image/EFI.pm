9e091855d5129092d96e8d30da6f89590a0de383
Valentin Gehrke <valentin.gehrke@kernkonzept.com>
2023-07-31 00:00:00 +0000

    L4::Image::EFI - Fix off by one error in sanity check
    Of course the raw_addr + raw_size can match the end_of_image if a
    section is right at the end of the file. The raw_addr can also match the
    end_of_image if the last section is empty.
    
    Change-Id: I8c746acb335b1bb383b951517f7e6bbd278a02b8

--- tool/lib/L4/Image/EFI.pm
+++ tool/lib/L4/Image/EFI.pm
@@ -385,8 +385,8 @@ sub objcpy_finalize
         {
           $section->{raw_addr} += $addr_delta;
         }
-      die "Internal error" unless $section->{raw_addr} < $end_of_image;
-      die "Internal error" unless $section->{raw_addr} + $section->{raw_size} < $end_of_image;
+      die "Internal error" unless $section->{raw_addr} <= $end_of_image;
+      die "Internal error" unless $section->{raw_addr} + $section->{raw_size} <= $end_of_image;
     }
 
   ## Write PE header
