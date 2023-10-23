ca9ed06e0513abfee8bd4bc99b56a9dd38d9e6a0
Valentin Gehrke <valentin.gehrke@kernkonzept.com>
2023-10-23 00:00:00 +0000

    L4::TapWrapper::Util - Get process tree via procfs
    Up until now we either had to have an extra dependency or rely on
    pstree to clean up all subprocesses. But we don't want  neither a
    further dependency nor rely on a tool which doesn't give the necessary
    information in a machine-readable format. To avoid both, we add code
    that retrieves the relevant information directly from the procfs, as
    the tools we did rely on would also do.
    
    Change-Id: Ieb262b0417f20118c20edb34e404f3b6f82d92a5

--- tool/lib/L4/TapWrapper/Util.pm
+++ tool/lib/L4/TapWrapper/Util.pm
@@ -8,28 +8,86 @@ use vars qw(@ISA @EXPORT);
 @ISA    = qw(Exporter);
 @EXPORT = qw(kill_ps_tree);
 
+sub cat {
+  my $file = shift;
+  local $/ = undef;
+  open my $fh, "<", $file or return undef;
+  <$fh>;
+};
+
+sub get_pidmap
+{
+  opendir(my $proc_dh, "/proc");
+
+  # Map parent -> child
+  my %pidmap;
+  # Map process -> state
+  my %pidstate;
+
+  while (my $pid = readdir($proc_dh))
+    {
+      next unless $pid =~ /^[0-9]+$/ && -d "/proc/$pid";
+
+      # Turns $pid into integer
+      $pid = 0+ $pid;
+
+      my $stat = cat("/proc/$pid/stat");
+
+      # process might have terminated since we called readdir
+      next unless defined $stat;
+
+      # The stat file's second field contains the cmdline wrapped in
+      # parenthesis. This poses two problems when parsing it: The cmdline can
+      # contain spaces, so we cannot just split the contents by whitespace
+      # characters, and it may contain parenthesis too, so we can't just remove
+      # it using a simple regex. The solution to this is removing everything
+      # until the last closing parenthesis in the file plus the subsequent
+      # whitespaces. Then the rest can be split by whitespace. This step removes
+      # the first two fields, so info contains the fields starting with the 3rd
+      # field: $info[0] is the running state (R/S/Z/...) and $info[1] is the
+      # parent pid.
+      $stat =~ s/^.+\)\s+([^\)]+)$/$1/;
+
+      my @info = split /\s+/, $stat;
+
+      # process state (R/S/Z/...)
+      $pidstate{$pid} = $info[0];
+
+      # parent pid
+      my $ppid = 0+ $info[1];
+
+      # Add an empty list of children unless there's already a list
+      $pidmap{$ppid} = [] unless exists $pidmap{$ppid};
+
+      # Add child to list
+      push @{$pidmap{$ppid}}, $pid;
+    }
+
+  closedir $proc_dh;
+
+  return (\%pidmap, \%pidstate);
+}
+
 sub get_ps_tree
 {
   my ($pid) = @_;
 
-  return () unless $pid && $pid > 1;
+  my ($pidmap,$pidstate) = get_pidmap();
 
-  my @pids = ($pid);
-  eval
+  my @processing = ($pid);
+  my @processed = ();
+
+  while (my $p = shift @processing)
     {
-      require Proc::Killfam;
-      require Proc::ProcessTable;
-      @pids = Proc::Killfam::get_pids([grep { $_->state ne 'defunct' } @{Proc::ProcessTable->new->table}], $pid);
-    };
-  if ($@) # no Proc::Killfam available; use external 'pstree';
-    {
-      # 'echo' turns deeper pstree output into one line;
-      # pids are in (parens), so 'split' on '()' and take every 2nd entry
-      my @pstree = map { split(/[()]/) } qx{echo \$(pstree -lp $pid)};
-      @pids = @pstree[grep $_ % 2, 0..$#pstree];
-      @pids = grep { system("ps -ax -o pid=,stat= | grep -Eq '\\<$_\\>\\s+Z\$'") != 0 } @pids; # ignore zombies
+      push @processing, @{$pidmap->{$p} || []};
+
+      # Only gather non-zombie processes
+      push @processed, $p unless $pidstate->{$p} eq "Z";
     }
-  return @pids;
+
+  shift @processed if @processed && $processed[0] == $pid; # Remove $pid;
+
+  return @processed;
 }
 
 # Kill a process tree beginning from the leaf nodes;
