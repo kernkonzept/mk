a012dd60c1e2d487ea16737c819516b68ec9d0fb
Valentin Gehrke <valentin.gehrke@kernkonzept.com>
2026-07-06 00:00:00 +0000

    TapWrapper/TAPOutput - Add support for several prefixed blocks
    For tests which print TAP from multiple sources, we introduced the
    BundleMode a while ago. The BundleMode, however, requires a certain
    synchronization on the test-side while TAP is being printed. In cases
    where such a synchronization is neither viable nor possible, we need a
    plugin that can handle several interleaved TAP blocks. The BundleMode
    however cannot run several subplugins at once, because there are plugins
    like OutputMatching, which don't work well if run multiple times
    simultaneously.
    
    The TAPOutput plugin supports one TAP block with or without a prefix.
    This change alters the TAPOutput plugin to support arbitrarily many
    TAP blocks, as long as these have different prefixes. As we need to know
    how many TAP blocks are expected to make sure we have collected all TAP
    blocks, we introduce an optional plugin parameter block_count to the
    TAPOutput plugin. This parameter defaults to 1, which mimics the former
    behaviour of the TAPOutput plugin.
    
    Change-Id: I624fa2b82d80fa1ac0280945cedfe22a2390cf8c
    Signed-off-by: Valentin Gehrke <valentin.gehrke@kernkonzept.com>

--- tool/lib/L4/TapWrapper/Plugin/TAPOutput.pm
+++ tool/lib/L4/TapWrapper/Plugin/TAPOutput.pm
@@ -8,34 +8,79 @@ use File::Basename;
 use parent 'L4::TapWrapper::Plugin';
 
 sub new {
+  my ($type, $args) = @_;
   my $self = L4::TapWrapper::Plugin->new();
+
+  $self->{args} = $args;
+  $self->{args}{block_count} //= 1;
+
+  L4::TapWrapper::fail_test(
+    "TAPOutput parameter block_count needs to be greater or equal to 1.")
+    unless $self->{args}{block_count} >= 1;
+
+  $self->{blocks_collected} = 0;
+  $self->{active_blocks} = {};
+
   $self->inhibit_exit(); # We need at least one TAP block
   return bless $self, shift;
 }
 
-# We use the legacy format to pass
-sub check_start {
+sub handle_block_start {
   my $self = shift;
   return unless shift =~ m/^(.*)TAP TEST START/;
-  $self->{block_prefix} = $1;
-  $self->{in_block} = 1;
+  my $prefix = $1;
+
+  L4::TapWrapper::fail_test(
+    "Several concurrent blocks with same prefix '$prefix'." .
+    "Make sure to use different prefixes")
+    if exists $self->{active_blocks}{$prefix};
+
+  $self->{active_blocks}{$prefix} = [];
+
+  L4::TapWrapper::fail_test("More TAP blocks found than anticipated.")
+    if ($self->{blocks_collected} + scalar(keys %{$self->{active_blocks}}) > $self->{args}{block_count});
+
+  return 1;
 }
 
-sub check_end {
+sub handle_block_end {
   my $self = shift;
-  return unless shift =~ m/TAP TEST FINISH/;
-  $self->{in_block} = 0;
-  $self->permit_exit();
+  return unless shift =~ m/^(.*)TAP TEST FINISH/;
+  my $prefix = $1;
+
+  L4::TapWrapper::fail_test("End of block with prefix '$prefix' that wasn't started.")
+    unless exists $self->{active_blocks}{$prefix};
+
+  $self->add_raw_tap_line(@{$self->{active_blocks}{$prefix}});
+
+  delete $self->{active_blocks}{$prefix};
+
+  $self->{blocks_collected}++;
+
+  $self->permit_exit()
+    if $self->{blocks_collected} == $self->{args}{block_count} &&
+    (keys %{$self->{active_blocks}} == 0);
+
+  return 1;
 }
 
-sub process_mine {
+sub process_any {
   my $self = shift;
   my $line = shift;
-  if ($line =~ s/^\Q$self->{block_prefix}\E//)
+
+  # strip color escapes
+  $line =~ s/\e\[[\d,;\s]+[A-Za-z]//gi;
+
+  return if $self->handle_block_end($line);
+  return if $self->handle_block_start($line);
+
+  foreach my $prefix (sort { length($b) <=> length($a) } keys %{$self->{active_blocks}})
     {
-      # strip color escapes
-      $line =~ s/\e\[[\d,;\s]+[A-Za-z]//gi;
-      $self->add_raw_tap_line($line);
+      next unless $line =~ s/^\Q$prefix\E//;
+
+      push @{$self->{active_blocks}{$prefix}}, $line;
+
+      last;
     }
 }
 
